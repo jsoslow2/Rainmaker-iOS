db02ad4f0a6039635cf04bdfd35ac5f8ffc51b1f
Jack Soslow <jsoslow2@gmail.com>
2018-11-16 19:04:40 -0500

    Xcode project

---
+++ Rainmaker/Rainmaker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Rainmaker
+//
+//  Created by Jack Soslow on 11/16/18.
+//  Copyright © 2018 Jack Soslow. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
