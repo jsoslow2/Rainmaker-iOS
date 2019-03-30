1d6c4b93b391afdaec67e54a1a5fd420759bb401
Jack Soslow <jsoslow2@gmail.com>
2019-03-30 14:45:25 -0400

    finished onboarding

--- Rainmaker/Rainmaker/Controllers/Login Flow/betForFreeViewController.swift
+++ Rainmaker/Rainmaker/Controllers/Login Flow/betForFreeViewController.swift
@@ -16,31 +16,14 @@ class betForFreeViewController : UIViewController {
     
     var username : String?
     
-    @IBOutlet weak var moneyLabel: UILabel!
     @IBOutlet weak var continueButton: UIButton!
     
-    let combination = NSMutableAttributedString()
-    var yourAttributes : Any?
-    var yourOtherAttributes : Any?
-    
-    let mintGreen = (UIColor(red: 0.494, green: 0.831, blue: 0.682, alpha: 1.0))
-    let badGrey = (UIColor(red: 0.937, green: 0.937, blue: 0.957, alpha: 1.0))
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
         continueButton.layer.cornerRadius = 20
         
-        
-        yourAttributes = [NSAttributedString.Key.foregroundColor: mintGreen, NSAttributedString.Key.font: UIFont(name: "Avenir", size: 45)]
-        yourOtherAttributes = [NSAttributedString.Key.foregroundColor: mintGreen, NSAttributedString.Key.font: UIFont(name: "Avenir", size: 14)]
-        let partOne = NSMutableAttributedString(string: String(100), attributes: yourAttributes as? [NSAttributedString.Key : Any])
-        let partTwo = NSMutableAttributedString(string: "drops", attributes: yourOtherAttributes as? [NSAttributedString.Key : Any])
-        
-        
-        combination.append(partOne)
-        combination.append(partTwo)
-        
-        moneyLabel.attributedText = combination
     }
     
     
@@ -52,23 +35,13 @@ class betForFreeViewController : UIViewController {
     
     @IBAction func continueButtonPressed(_ sender: Any) {
         
-        let usernameText = username
-        let firUser = Auth.auth().currentUser
+            
         
-        UserService.create(firUser!, usernameText: usernameText!) { (user) in
-            guard let user = user else { return }
-            
-            User.setCurrent(user, writeToUserDefaults: true)
-            
-            let initialViewController = UIStoryboard.initialViewController(for: .main)
-            self.view.window!.rootViewController = initialViewController
-            self.view.window?.makeKeyAndVisible()
-            
-            
-            
-            print("Created new user: \(user.username)")
-            
-        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let destinationVC = segue.destination as? FinalOnboardingViewController
+        destinationVC?.username = username
     }
     
 }
