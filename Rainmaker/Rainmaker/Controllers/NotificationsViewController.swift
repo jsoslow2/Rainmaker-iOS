57f6fe93784abbfbd1b25f322f084055bf94e138
Jack Soslow <jsoslow2@gmail.com>
2019-04-18 10:24:42 -0400

    made notifications tab

---
+++ Rainmaker/Rainmaker/Controllers/NotificationsViewController.swift
@@ -0,0 +1,20 @@
+//
+//  NotificationsViewController.swift
+//  Rainmaker
+//
+//  Created by Jack Soslow on 4/16/19.
+//  Copyright © 2019 Jack Soslow. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class NotificationsViewController : UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
